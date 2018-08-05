a2aa63a95b1052c2e0c164fce77f1805da78bea5
DESKTOP-O6S7FCJ\TvT <tvturnhout@gmail.com>
2018-08-05 12:44:25 +0200

    added ML

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,6 @@
 from sklearn.linear_model import SGDClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn import tree
 import json
 from random import shuffle
 
@@ -32,11 +34,25 @@ for item in data_new:
 X_test = X[::3]
 y_test = y[::3]
 X_train = [item for item in X if item not in X_test]
-y_train = [item for item in y if item not in y_test]
+y_train = []
+index_list = []
+for item in X_train:
+    index_list.append(X.index(item))
+for item in index_list:
+    y_train.append(y[item])
 
 
 # clf 
 clf = SGDClassifier(loss="hinge", penalty="l2")
 clf.fit(X_train, y_train)
+print( "SGD: " + str(clf.score(X_test, y_test)) )
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+print( "Tree: " + str(clf.score(X_test, y_test)) )
+
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+clf.fit(X_train, y_train)
+print( "MLP: " + str(clf.score(X_test, y_test)) )
 
 
